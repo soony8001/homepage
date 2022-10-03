7d63b7ba2f433371dc3969fe38eb4df94e2aabb3
soony8001 <soony8001@naver.com>
2022-10-03 23:52:36 +0900

    최초 등록

---
+++ src/main/java/main/module/VueAdminTemplateMasterApplication.java
@@ -0,0 +1,13 @@
+package main.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueAdminTemplateMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueAdminTemplateMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/main/module/VueAdminTemplateMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package main.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueAdminTemplateMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
